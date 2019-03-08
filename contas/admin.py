c0134316f50c3fe67c347ed1ada7352dcaba41c5
Kaique Prado <kprado@micromidia-1.local>
2019-03-08 09:46:19 -0300

    banco

--- contas/admin.py
+++ contas/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 # testar se deu certo o banco
 from .models import Categoria
+from .models import Transacao
 
 #Deixar disponivel no site /admin
 admin.site.register(Categoria)
+admin.site.register(Transacao)
